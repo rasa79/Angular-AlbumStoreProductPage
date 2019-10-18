1e4d3f4a4d05ae88273d586573fe8e76cea2d08d
Radovan  Radivojevic <rradivojevic@gmail.com>
2019-10-18 04:42:25 -0700

    importing http and defining variables

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,8 +1,11 @@
 import { Injectable } from '@angular/core';
+import { Http, Response } from '@angular/http'
 
 @Injectable()
 export class ProductService {
 
-  constructor() { }
+  private _albumUrl: '../assets/album.json'
+
+  constructor(private _http: Http) { }
 
 }
