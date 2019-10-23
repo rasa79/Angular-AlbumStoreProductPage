e77592f8114c81bdcadcf33c509d0412e6613144
Radovan  Radivojevic <rradivojevic@gmail.com>
2019-10-23 13:59:12 +0200

    subscription to the service

--- src/app/product-description/product-description.component.ts
+++ src/app/product-description/product-description.component.ts
@@ -9,9 +9,15 @@ import { ProductService } from '../product.service';
 })
 export class ProductDescriptionComponent implements OnInit {
 
+  albumInfo;
+
   constructor(private _productService: ProductService) { }
 
   ngOnInit() {
+    this._productService.getAlbum(1)
+    .subscribe(
+      response => this.albumInfo = response)
+    )
   }
 
 }
